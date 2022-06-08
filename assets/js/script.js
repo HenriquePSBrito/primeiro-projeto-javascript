d43a518c7ffa8a48c28f527bb14a8efb1313e166
HenriquePSBrito <jhowhp@yahoo.com.br>
2022-06-08 19:51:10 -0300

    addEventListener e add ajustes css

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,24 @@ var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
 
 function increment(){
-    currentNumber = currentNumber + 1;
+    currentNumber += 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
 
 function decrement(){
-    currentNumber = currentNumber - 1;
+    currentNumber -= 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
+
+var counter = document.querySelector("#counter");
+
+counter.addEventListener("click", function() {
+
+    if (currentNumber < 0) {
+        this.style.color = "red";
+    }
+
+    else 
+        this.style.color = "blue";
+
+});
